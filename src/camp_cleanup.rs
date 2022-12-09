7a92c5bd133ebb168e67ea61c7516da7ca273b4b
samirm <samir.m8@gmail.com>
2022-12-09 17:29:36 -0500

    day4 done

--- src/camp_cleanup.rs
+++ src/camp_cleanup.rs
@@ -1,9 +1,9 @@
+use std::collections::HashSet;
 
 fn main() {
     let file_as_string: String = std::fs::read_to_string("data/camp.txt").expect("no such file").parse().expect("could not parse");
     let lines: Vec<&str> = file_as_string.lines().collect();
 
-    // let mut rucksacks: Vec<HashSet<char>> = vec!();
     let mut total = 0;
 
     for line in lines {
@@ -16,19 +16,32 @@ fn main() {
             .map(|n| n.parse().unwrap())
             .collect::<Vec<i32>>();
 
-        if first_elf[1] - first_elf[0] <= second_elf[1] - second_elf[0] {
-            if first_elf[0] >= second_elf[0] && first_elf[1] <= second_elf[1] {
-                total += 1
-            } else {
-                continue
-            }
-        } else if second_elf[1] - second_elf[0] <= first_elf[1] - first_elf[0] {
-            if second_elf[0] >= first_elf[0] && second_elf[1] <= first_elf[1] {
-                total += 1
-            } else {
-                continue
+        // if first_elf[1] - first_elf[0] <= second_elf[1] - second_elf[0] {
+        //     if first_elf[0] >= second_elf[0] && first_elf[1] <= second_elf[1] {
+        //         total += 1
+        //     } else {
+        //         continue
+        //     }
+        // } else if second_elf[1] - second_elf[0] <= first_elf[1] - first_elf[0] {
+        //     if second_elf[0] >= first_elf[0] && second_elf[1] <= first_elf[1] {
+        //         total += 1
+        //     } else {
+        //         continue
+        //     }
+        // }
+
+        let mut set1 = HashSet::new();
+
+        for i in first_elf[0]..first_elf[1]+1 {
+            // println!("{}", i);
+            set1.insert(i);
+        }
+        for i in second_elf[0]..second_elf[1]+1 {
+            if set1.contains(&i) {
+                total += 1;
+                break
             }
         }
     }
-    println!("part1: {:?}", total);
+    println!("part2: {:?}", total);
 }
