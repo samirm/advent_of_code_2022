e50adc5ca1b761e14c9a665013265ce04fb9ad71
samirm <samir.m8@gmail.com>
2022-12-12 22:51:19 -0500

    day6 done

--- src/comms.rs
+++ src/comms.rs
@@ -6,16 +6,14 @@ fn main() {
     let chars: Vec<char> = lines[0].chars().collect();
 
     let mut start_of_packet = HashSet::new();
-    let mut index = 0;
+    const PACKET_SIZE: usize = 14;
 
-    for i in 4..chars.len() {
-        start_of_packet = HashSet::from_iter(&chars[i-4..i]);
+    for i in PACKET_SIZE..chars.len() {
+        start_of_packet = HashSet::from_iter(&chars[i-PACKET_SIZE..i]);
         println!("{:?}", start_of_packet);
-        if start_of_packet.len() == 4 {
-            println!("yes");
-            index = i;
+        if start_of_packet.len() == PACKET_SIZE {
+            println!("index: {}", i);
             break
         }
     }
-    println!("{}", index);
 }
